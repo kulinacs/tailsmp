95e282b1dba4f162f3325b538bbeffda95379956
Debian Live user <amnesia@localhost.localdomain>
2016-04-16 22:42:31 +0000

    rearranged structure of scripts

---
+++ readPreferences.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf /mnt/private/TailsData_unlocked
+mv /live/persistence/TailsData_unlocked /mnt/private
--- writePreferences.sh
+++ writePreferences.sh
@@ -2,3 +2,4 @@
 sudo cryptsetup luksOpen /dev/sdc1 profile
 sudo mkdir /mnt/private
 sudo mount -t ext4 /dev/mapper/profile /mnt/private
+mv /mnt/private/TailsData_unlocked /live/persistence/tailsData_unlocked
