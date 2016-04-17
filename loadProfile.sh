95a10aa341ffa3f0fa1a33493b07e360b1a42fe6
jachan <john7chan@gmail.com>
2016-04-17 02:37:48 +0000

    fixed chown problems

--- loadProfile.sh
+++ loadProfile.sh
@@ -4,7 +4,7 @@ sudo cryptsetup luksOpen /dev/sdc profile
 # mounts SD card to private
 sudo mkdir /mnt/private
 sudo mount -t ext4 /dev/mapper/profile /mnt/private
-sudo chown amnesia.amnesia /mnt/private
+sudo chown -R /home
 
 # copies the data from the SD card back to home
-cp /mnt/private/home /
+cp -rP /mnt/private/amnesia /home/
