6e95587862b7f8c98d90fe33320e0875a7f92a5c
kulinacs <kulinacs@bellicosedev.com>
2016-04-16 22:05:09 -0500

    Cleanup

---
+++ scripts/configure-server.sh
@@ -0,0 +1,3 @@
+echo -n "Enter your username: "
+read username
+ssh-keygen -t ed25519 -o -a 100 -C "$username" << LOL << lol < lol
---
+++ scripts/make-role.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$1" ]
+  then
+    echo "A folder name must be specifed"
+	exit
+fi
+
+mkdir -P "roles/$1/defaults"
+mkdir "roles/$1/handlers"
+mkdir "roles/$1/meta"
+mkdir "roles/$1/tasks"
+mkdir "roles/$1/templates"
